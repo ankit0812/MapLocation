5f4a91b94a33d9986ccf3909a834d1ebcbdf106e
ankitoptimus <ankitkr.gupta@optimusinfo.com>
2015-07-20 19:21:34 +0530

    Map App Part 1- Map along with longitude latitude and altitude and shows current location , Running perfectly on all os versions , Screenshots attached. Segmented tab is to be added

---
+++ MapLocation/ViewController.h
@@ -0,0 +1,28 @@
+//
+//  ViewController.h
+//  MapLocation
+//
+//  Created by optimusmac4 on 7/20/15.
+//  Copyright (c) 2015 optimusmac4. All rights reserved.
+//
+#import <MapKit/MapKit.h>
+#import <CoreLocation/CoreLocation.h>
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (nonatomic, strong) CLLocationManager *locationManager;
+
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+
+@property (weak, nonatomic) IBOutlet UILabel *labelLatitude;
+
+@property (weak, nonatomic) IBOutlet UILabel *labelLongitude;
+
+@property (weak, nonatomic) IBOutlet UILabel *labelAltitude;
+
+//- (IBAction)setMap:(id)sender;
+
+
+@end
+
---
+++ MapLocation/ViewController.m
@@ -0,0 +1,81 @@
+//
+//  ViewController.m
+//  MapLocation
+//
+//  Created by optimusmac4 on 7/20/15.
+//  Copyright (c) 2015 optimusmac4. All rights reserved.
+//
+#define METERS_MILE 1609.344
+#define METERS_FEET 3.28084
+#import "ViewController.h"
+
+@interface ViewController ()
+
+<CLLocationManagerDelegate>
+
+@end
+
+@implementation ViewController
+
+//@synthesize mapView=_mapView;
+
+
+-(void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [[self mapView] setShowsUserLocation:YES];
+    
+    self.locationManager = [[CLLocationManager alloc] init];
+    
+    [[self locationManager] setDelegate:self];
+    
+    // we have to setup the location maanager with permission in later iOS versions
+    if ([[self locationManager] respondsToSelector:@selector(requestWhenInUseAuthorization)]) {
+        [[self locationManager] requestWhenInUseAuthorization];
+    }
+    
+    [[self locationManager] setDesiredAccuracy:kCLLocationAccuracyBest];
+    [[self locationManager] startUpdatingLocation];
+}
+
+
+
+
+-(void) locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations {
+    CLLocation *location = locations.lastObject;
+    [[self labelLatitude] setText:[NSString stringWithFormat:@"%.6f", location.coordinate.latitude]];
+    [[self labelLongitude] setText:[NSString stringWithFormat:@"%.6f", location.coordinate.longitude]];
+    [[self labelAltitude] setText:[NSString stringWithFormat:@"%.2f feet", location.altitude*METERS_FEET]];
+    
+    // zoom the map into the users current location
+    MKCoordinateRegion viewRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, 2*METERS_MILE, 2*METERS_MILE);
+    [[self mapView] setRegion:viewRegion animated:YES];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*- (IBAction)setMap:(id)sender {
+    
+    switch (((UISegmentedControl *)sender).selectedSegmentIndex)
+    {
+        case 0:
+        _mapView.mapType=MKMapTypeStandard;
+        break;
+            
+        case 1:
+        _mapView.mapType=MKMapTypeSatellite;
+        break;
+
+        case 2:
+        _mapView.mapType=MKMapTypeHybrid;
+        break;
+
+            
+        default:
+            break;
+    }
+}*/
+@end
